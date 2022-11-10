ba20716dabb03726c5ff732866fda9d9e7f8a5ff
Quang Quan <nguyenquangquan_t65@hus.edu.vn>
2022-11-10 21:44:02 +0700

    lần đầu commit

---
+++ DuAnCoBan/ClockRealTime/JSwing.java
@@ -0,0 +1,15 @@
+package DuAnCoBan.ClockRealTime;
+
+import javax.swing.*;
+
+public class JSwing extends JFrame {
+    public JSwing(){
+        JFrame frame = new JFrame();
+        frame.setSize(4000,350);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JSwing();
+    }
+}
